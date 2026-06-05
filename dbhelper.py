26b5b57d89af6a1a427a1cd09dc356c0ea90d038
Mahesh Singh <maheshbeniwal74@gmail.com>
2026-06-05 23:42:27 +0530

    Switch database configuration to Render Postgres

--- dbhelper.py
+++ dbhelper.py
@@ -1,27 +1,22 @@
-import mysql.connector
+import psycopg2
 
 class DB:
     def __init__(self):
         # connect to the database
         try:
-            self.conn = mysql.connector.connect(
-                host='database-1.codzmntflx6t.ap-northeast-1.rds.amazonaws.com',
-                user='admin',
-                password='911Pentagon',
-                database='flights'
-            )
+            # Using your Render Internal Database URL
+            self.conn = psycopg2.connect("postgresql://admin:8s8njfthGPcPfmihNoFGOvf7qkleaGu6@dpg-d8hgnof7f7vs73chs7ag-a/flights_9aks")
             self.mycursor = self.conn.cursor()
             print('Connection established')
-        except:
-            print('Connection error')
+        except Exception as e:
+            print(f'Connection error: {e}')
 
     def fetch_city_names(self):
-
         city = []
         self.mycursor.execute("""
-        SELECT DISTINCT(Destination) FROM flights.flights
+        SELECT DISTINCT("Destination") FROM flights
         UNION
-        SELECT DISTINCT(Source) FROM flights.flights
+        SELECT DISTINCT("Source") FROM flights
         """)
 
         data = self.mycursor.fetchall()
@@ -31,25 +26,23 @@ class DB:
 
         return city
 
-    def fetch_all_flights(self,source,destination):
-
+    def fetch_all_flights(self, source, destination):
+        # Using secure parameterized inputs to prevent SQL errors with strings
         self.mycursor.execute("""
-        SELECT Airline,Route,Dep_Time,Duration,Price FROM flights
-        WHERE Source = '{}' AND Destination = '{}'
-        """.format(source,destination))
+        SELECT "Airline", "Route", "Dep_Time", "Duration", "Price" FROM flights
+        WHERE "Source" = %s AND "Destination" = %s
+        """, (source, destination))
 
         data = self.mycursor.fetchall()
-
         return data
 
     def fetch_airline_frequency(self):
-
         airline = []
         frequency = []
 
         self.mycursor.execute("""
-        SELECT Airline,COUNT(*) FROM flights
-        GROUP BY Airline
+        SELECT "Airline", COUNT(*) FROM flights
+        GROUP BY "Airline"
         """)
 
         data = self.mycursor.fetchall()
@@ -58,18 +51,20 @@ class DB:
             airline.append(item[0])
             frequency.append(item[1])
 
-        return airline,frequency
+        return airline, frequency
 
     def busy_airport(self):
-
         city = []
         frequency = []
 
+        # Standardized the inner column alias so GROUP BY works perfectly in Postgres
         self.mycursor.execute("""
-        SELECT Source,COUNT(*) FROM (SELECT Source FROM flights
-							UNION ALL
-							SELECT Destination FROM flights) t
-        GROUP BY t.Source
+        SELECT t.airport, COUNT(*) FROM (
+            SELECT "Source" AS airport FROM flights
+            UNION ALL
+            SELECT "Destination" AS airport FROM flights
+        ) t
+        GROUP BY t.airport
         ORDER BY COUNT(*) DESC
         """)
 
@@ -82,13 +77,12 @@ class DB:
         return city, frequency
 
     def daily_frequency(self):
-
         date = []
         frequency = []
 
         self.mycursor.execute("""
-        SELECT Date_of_Journey,COUNT(*) FROM flights
-        GROUP BY Date_of_Journey
+        SELECT "Date_of_Journey", COUNT(*) FROM flights
+        GROUP BY "Date_of_Journey"
         """)
 
         data = self.mycursor.fetchall()
